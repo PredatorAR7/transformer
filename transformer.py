2c5477e1553e5448d9b14fa6d2102f7892d777c6
PredatorAR7 <adityaraj@vipas.ai>
2024-06-01 18:53:45 +0530

    v4

--- transformer.py
+++ transformer.py
@@ -10,7 +10,7 @@ def softmax(x):
    return e_x / e_x.sum(axis=0)
 
 
-def transform(input):
+def pre_transform(input):
    print("In Custom pre_transform method")
    # Displaying initial part of the input for debugging
    print(str(input)[:20], "...", str(input)[-20:])
@@ -25,4 +25,24 @@ def transform(input):
    return transformed_input
 
 
+def post_transform(input_scores):
+   print("In Custom post_transform method")
+   # Displaying initial part of the input scores for debugging
+   print(str(input_scores)[:20], "...", str(input_scores)[-20:])
+   class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+   # Apply softmax to the input scores
+   probabilities = softmax(np.array(input_scores))
+   # Find the index of the maximum score/probability
+   class_index = np.argmax(probabilities)
+   class_name = class_names[class_index]
+   confidence = 100 * probabilities[class_index]
+   # Convert confidence to float for consistency
+   confidence_value = float(confidence)
+   processed_response = (class_name, confidence_value)
+   print("Custom post transformation done")
+   # Displaying processed response for debugging
+   print(str(processed_response)[:200], "...", str(processed_response)[-200:])
+   print(class_name)
+   print(confidence)
+   return processed_response
 
