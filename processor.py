a9185db00dd8cdacceafc2ab5482832c317975d3
Aditya Raj Raizada <161312262+PredatorAR7@users.noreply.github.com>
2024-07-16 16:10:55 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,28 +1,24 @@
 import numpy as np
-from PIL import Image # nosec
+from PIL import Image  # nosec
 from io import BytesIO
-import base64 # nosec
+import base64  # nosec
 import requests
 from boto3 import Session
 
-def softmax(x): #nosec
-   """Compute softmax values for each set of scores in x."""
-   e_x = np.exp(x - np.max(x))scores in x."""scores in x."""
-   e_x = np.exp(x - np.max(x))
-   e_x = np.exp(x - np.max(x))
-   return e_x / e_x.sum(axis=0)
+def softmax(x):  # nosec
+    """Compute softmax values for each set of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
 
 def predict(input):
-   pass
-
+    pass
 
 def pre_process(input):
-   # Displaying initial part of the input for debugging
-   img_height = 180
-   img_width = 180
-   # Decode the input, load as an image, resize, and convert to a NumPy array
-   img_array = np.array(Image.open(BytesIO(base64.b64decode(input))).resize((img_height, img_width)))
-   transformed_input = img_array.tolist()
-   # Displaying initial part of the transformed input for debugging
-
-
+    # Displaying initial part of the input for debugging
+    img_height = 180
+    img_width = 180
+    # Decode the input, load as an image, resize, and convert to a NumPy array
+    img_array = np.array(Image.open(BytesIO(base64.b64decode(input))).resize((img_height, img_width)))
+    transformed_input = img_array.tolist()
+    # Displaying initial part of the transformed input for debugging
+    return transformed_input
